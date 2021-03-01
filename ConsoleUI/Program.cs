42a0183b95786121155992b0415051750bcc7576
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-03-01 18:08:24 +0200

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLID
+    //Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(50,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
