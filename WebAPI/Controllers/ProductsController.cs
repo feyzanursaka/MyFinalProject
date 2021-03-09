479ea49576cc94343d9b1705245083c2af145a38
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-03-10 00:03:59 +0200

    13 Şubat 1. Commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Dependency chain
+        //Loosely coupled
+        //IoC Container -- Inversion of control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {  
+            
+            var result = _productService.GetAll();
+
+            return result.Data;
+        }
+    }
+}
